8bf53c9dea284a51ffaf149e776f752fa3b9767f
Wvirgil123 <1156386088@qq.com>
2023-05-06 11:26:43 +0800

    add adc example

---
+++ examples/adc/adc.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#define ADC1  27
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int adc0_data = analogRead(ADC1);
+  Serial.println(adc0_data);
+  delay(1000);
+}
