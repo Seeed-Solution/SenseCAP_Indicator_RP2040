a1ba988b791acb4f1e56981a71a535a241cd93e2
Wvirgil123 <1156386088@qq.com>
2023-05-06 11:27:24 +0800

    add co2 example

---
+++ examples/co2/co2.ino
@@ -0,0 +1,84 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SensirionI2CScd4x.h>
+
+
+SensirionI2CScd4x scd4x;
+String SDDataString = "";
+
+//The built-in sensor needs to be powered on
+void sensor_power_on(void) {
+  pinMode(18, OUTPUT);
+  digitalWrite(18, HIGH);
+}
+
+void sensor_scd4x_init(void) {
+  uint16_t error;
+  char errorMessage[256];
+
+  scd4x.begin(Wire);
+
+  // stop potentially previously started measurement
+  error = scd4x.stopPeriodicMeasurement();
+  if (error) {
+    Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+
+  // Start Measurement
+  error = scd4x.startPeriodicMeasurement();
+  if (error) {
+    Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+}
+
+void sensor_scd4x_get(void) {
+  uint16_t error;
+  char errorMessage[256];
+
+  Serial.print("sensor scd4x: ");
+  // Read Measurement
+  uint16_t co2;
+  float temperature;
+  float humidity;
+  error = scd4x.readMeasurement(co2, temperature, humidity);
+  if (error) {
+    Serial.print("Error trying to execute readMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  } else if (co2 == 0) {
+    Serial.println("Invalid sample detected, skipping.");
+  } else {
+    Serial.print("Co2:");
+    Serial.print(co2);
+    Serial.print("\t");
+    Serial.print("Temperature:");
+    Serial.print(temperature);
+    Serial.print("\t");
+    Serial.print("Humidity:");
+    Serial.println(humidity);
+  }
+}
+
+int cnt = 0;
+void setup() {
+  Serial.begin(115200);
+
+  sensor_power_on();
+
+  Wire.setSDA(20);
+  Wire.setSCL(21);
+  Wire.begin();
+
+  sensor_scd4x_init();
+}
+
+void loop() {
+  delay(5000);
+  sensor_scd4x_get();
+}
